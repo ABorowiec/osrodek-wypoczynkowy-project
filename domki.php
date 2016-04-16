da9d9921e19a05d36de228a22081c6826f71b775
ABorowiec <boro235@gmail.com>
2016-04-16 19:31:03 +0200

    Update domki.php

--- domki.php
+++ domki.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require_once('config.php');
+
+
 ?>
 
 <html>
@@ -11,7 +13,7 @@ require_once('config.php');
 </head>
 <body>
 <div id=site>
-<div id=mainsite style ="width: 850px; top: 100px; left: 100px; position: absolute; border: 1px; border-style: solid; border-color: #ffffff;">
+<div id=mainsite style ="width: 850px; height: 1040px; top: 100px; left: 100px; position: absolute; border: 1px; border-style: solid; border-color: #ffffff;">
 
 <?php
 
@@ -24,20 +26,142 @@ $user = $_SESSION['username'];
 	//$connection=mysql_connect ($db_host, $db_user, $db_pass) or die ("Próba połączenie z bazą danych nie powiodła się. Spróbuj ponownie");
 	//mysql_select_db($db_name);
 	//$level_z_bazy = mysql_query("SELECT level FROM users WHERE username = '$user'");
+	
 
 	if ($level == 6 || $level == 5 || $level == 4 || $level == 2 )
 	{
-	echo "<h1>Dodaj do bazy</h1>";
-	echo "<form method='post' action=''>";
-	echo "Pole:<input type='text' name='marka' size='15' /> <br/><br/>";
-	echo "Pole 2<input type='text' name='poj' size='15' /> <br/><br/>";
-	echo "Pole 3:<input type='text' name='konie' size='15' /> <br/><br/>";
-	echo "Pole 4:<input type='text' name='nr' size='15' /> <br/><br/>";
-	echo "Pole 5:<textarea name='l' rows=4 cols=18></textarea><br/><br/>";
-	echo "<input type='submit' name='submit' value='Dodaj' />";
-	echo "</form>";
+	
+	$connection=mysql_connect ($db_host, $db_user, $db_pass) or die ("Próba połączenie z bazą danych nie powiodła się. Spróbuj ponownie");
+	mysql_select_db($db_name);
+	$wszystkie = mysql_query("SELECT * FROM domek ORDER BY Nazwa ASC");
+	
+	//pejdżowanie
+	
+	$ilosc = mysql_num_rows($wszystkie);
+	
+	$na_stronie = 5;
+
+
+	$ostatnia_strona = ceil($ilosc/$na_stronie);
+		
+		if (!isset($_GET['pagenum']))
+		{
+		$pagenum = 1;
+		}
+		else
+		{
+		$pagenum = $_GET['pagenum'];
+		}
+
+		
+			if ($pagenum < 1)
+			{
+			$pagenum = 1;
+			}
+			elseif ($pagenum > $ostatnia_strona)
+			{
+			$pagenum = $ostatnia_strona;
+			}
+			$offset = ($pagenum -1) * $na_stronie;
+
+		
+		
+		$data = "SELECT * FROM domek ORDER BY Nazwa ASC LIMIT $na_stronie OFFSET $offset";
+
+		$lista = mysql_query($data);
+	
+	while ($row = mysql_fetch_row($lista))
+	{
+	echo "<table border=1><tr>";
+	echo "<td>". $row[1] ."</td>";
+	echo "<td>". $row[2] ."</td>";
+	echo "</tr></table>";
 	}
-	else
+	
+	echo"<br/><br/>";
+	echo"<br/><br/>";
+
+		echo "Strona ".$pagenum." z ".$ostatnia_strona." ";
+
+
+		if ($pagenum == 1)
+		{
+
+		} 
+		else
+		{
+
+		echo"<a href='?pagenum=1'>Pierwsza strona</a>";
+		echo" ";
+		$previous = $pagenum - 1;
+		echo"<a href='?pagenum=$previous'>Poprzednia strona</a>";
+		}
+		if ($pagenum == $ostatnia_strona)
+		{
+
+		}
+		else
+		{
+		$next = $pagenum + 1;
+		echo"<a href='?pagenum=$next'>Następna strona</a>";
+		echo" ";
+		echo"<a href='?pagenum=$ostatnia_strona'>Ostatnia strona</a>";
+		}
+	
+		
+	
+	// koniec pejdżowania
+	
+	echo"<br/><br/>";
+	echo"<br/><br/>";
+	
+	mysql_data_seek($lista, 0);
+	
+	echo"<form method='post' action=''>";
+	echo"<SELECT name='lista'>";
+	while ($row2 = mysql_fetch_row($lista))
+	{
+	echo "<option value='". $row2[1] ."'>". $row2[1] ."</option>";
+	}
+	echo"</SELECT>";
+	echo"<input type='submit' name='dodaj' value='Dodaj' />";
+	echo"<input type='submit' name='edytuj' value='Edytuj' />";
+	echo"<input type='submit' name='usun' value='Usuń' />";
+	echo"</form>";
+	
+		
+		
+		if (isset($_POST['dodaj']))
+		{
+		
+			echo "<h1>Dodaj do bazy</h1>";
+			echo "<form method='post' action=''>";
+			echo "Nazwa: <input type='text' name='nazwa' size='15' /> <br/><br/>";
+			echo "Opis: <input type='text' name='opis' size='15' /> <br/><br/>";
+			echo "<input type='submit' name='submit' value='Dodaj rekord' />";
+			echo "</form>";
+		}
+		if (isset($_POST['edytuj']))
+		{
+			
+			echo "<h1>Edytuj z bazy</h1>";
+			echo "<form method='post' action=''>";
+			echo "Nazwa: <input type='text' name='nazwa' size='15' /> <br/><br/>";
+			echo "Opis: <input type='text' name='opis' size='15' /> <br/><br/>";
+			echo "<input type='submit' name='submit' value='Edytuj rekord' />";
+			echo "</form>";
+			
+		}
+		if (isset($_POST['usun']))
+		{
+			echo "<h1>Usuń z bazy</h1>";
+			
+			
+			
+		}
+		
+	}
+	else	
 	{
 	echo "<b>Nieodpowiednie uprawnienia. Zostaniesz przekierowany w ciągu 5 sekund...</b>";
 	header('Refresh: 5; url=domki_panel_glowny.php');
